3317aa6531b661d45330bbd0040bff351b97011a
조은누리 <jounuli@naver.com>
2023-01-26 08:59:20 +0900

    commit

---
+++ Programmers/Heap.py
@@ -0,0 +1,42 @@
+class MaxHeap:
+
+    def __init__(self):
+        self.data = [None]
+
+    def insert(self, item):
+        self.data.append(item)
+        idx = len(self.data) - 1
+        while idx != 1:
+            if self.data[idx // 2] < self.data[idx]:
+                self.data[idx // 2], self.data[idx] = self.data[idx], self.data[idx // 2]
+                idx //= 2
+            else:
+                break
+
+    def remove(self):
+        if len(self.data) > 1:
+            self.data[1], self.data[-1] = self.data[-1], self.data[1]
+            data = self.data.pop(-1)
+            self.maxHeapify(1)
+        else:
+            data = None
+        return data
+
+    def maxHeapify(self, i):
+        left = 2 * i
+        right = 2 * i + 1
+        smallest = i
+
+        if left < len(self.data) and self.data[left] > self.data[smallest]:
+            smallest = left
+
+        if right < len(self.data) and self.data[right] > self.data[smallest]:
+            smallest = right
+
+        if smallest != i:
+            self.data[smallest], self.data[i] = self.data[i], self.data[smallest]
+            self.maxHeapify(smallest)
+
+
+def solution(x):
+    return 0
