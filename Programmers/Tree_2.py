0dbda26150e7e7beba9b49c110cd1d4f563b5685
jo-eunnuli <jounuli@naver.com>
2023-01-25 11:57:42 +0900

    commit

--- Programmers/Tree_2.py
+++ Programmers/Tree_2.py
@@ -1,3 +1,23 @@
+class ArrayQueue:
+
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def enqueue(self, item):
+        self.data.append(item)
+
+    def dequeue(self):
+        return self.data.pop(0)
+
+    def peek(self):
+        return self.data[0]
+
 class Node:
 
     def __init__(self, item):
@@ -60,6 +80,21 @@ class BinaryTree:
             return self.root.postorder()
         else:
             return []
+
+
+    def bft(self):
+        traversal = []
+        q = ArrayQueue()
+        if self.root:
+            q.enqueue(self.root)
+        while q.size():
+            node = q.dequeue()
+            if node.left:
+                q.enqueue(node.left)
+            if node.right:
+                q.enqueue(node.right)
+            traversal.append(node.data)
+        return traversal
 
 
 def solution(x):
